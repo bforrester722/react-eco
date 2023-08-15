1fa02a6e934d1992aa51a1dba282b270302e4802
bforrester722 <b.forrester@live.com>
2023-08-14 22:43:10 -0500

    adds styled-components library and replaces css files with styled logic

--- src/todos/NewTodoForm.tsx
+++ src/todos/NewTodoForm.tsx
@@ -1,28 +1,56 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 import { addTodoRequest } from "./thunks";
 import { getTodos } from "./selectors";
+import { createTodo } from "./actions";
 import { IState, ITodo } from "./interface";
-import "./NewTodoForm.css";
-import { Dispatch } from "redux";
 
 interface IProps {
   todos: ITodo[];
   onCreatePressed: (val: string) => void;
 }
+
+const FormContainer = styled.div`
+  border-radius: 8px;
+  padding: 16px;
+  text-align: center;
+  box-shadow: 0 4px 8px grey;
+`;
+
+const NewTodoInput = styled.input`
+  font-size: 16px;
+  padding: 8px;
+  border: none;
+  border-bottom: 2px solid #ddd;
+  border-radius: 8px;
+  width: 70%;
+  outline: none;
+`;
+
+const NewTodoButton = styled.button`
+  font-size: 16px;
+  padding: 8px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  cursor: pointer;
+  margin-left: 8px;
+  width: 20%;
+  background-color: #22ee22;
+`;
 const NewTodoForm = ({ todos, onCreatePressed }: IProps) => {
   const [inputValue, setInputValue] = useState("");
 
   return (
-    <div className="new-todo-form">
-      <input
-        className="new-todo-input"
+    <FormContainer>
+      <NewTodoInput
         type="text"
         placeholder="Type your new todo here"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <button
+      <NewTodoButton
         onClick={() => {
           const isDuplicateText = todos.some(
             (todo) => todo.text === inputValue
@@ -32,11 +60,10 @@ const NewTodoForm = ({ todos, onCreatePressed }: IProps) => {
             setInputValue("");
           }
         }}
-        className="new-todo-button"
       >
         Create Todo
-      </button>
-    </div>
+      </NewTodoButton>
+    </FormContainer>
   );
 };
 
@@ -46,7 +73,7 @@ const mapStateToProps = (state: IState) => {
   };
 };
 
-const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+const mapDispatchToProps = (dispatch: typeof createTodo) => ({
   onCreatePressed: (text: string) => dispatch(addTodoRequest(text)),
 });
 
--- src/todos/TodoListItem.tsx
+++ src/todos/TodoListItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./TodoListItem.css";
+import styled from "styled-components";
 import { ITodo } from "./interface";
 
 interface IProps {
@@ -8,30 +8,88 @@ interface IProps {
   onCompletedPressed: (val: string) => void;
 }
 
+const TodoItemContainer = styled.div`
+  background: #fff;
+  border-radius: 8px;
+  margin-top: 8px;
+  padding: 16px;
+  position: relative;
+  box-shadow: 0 4px 8px grey;
+`;
+
+export const getBorderStyleForDate = (
+  startingDate: Date,
+  currentDate: number
+) =>
+  startingDate > new Date(currentDate - 86400000 * 5)
+    ? "none"
+    : "2px solid red";
+
+const TodoItemContainerWithWarning = styled(TodoItemContainer)`
+  border-bottom: ${(props: ITodo) =>
+    new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5)
+      ? "none"
+      : "2px solid red"};
+`;
+
+const ButtonsContainer = styled.div`
+  position: absolute;
+  right: 12px;
+  bottom: 12px;
+`;
+
+const Button = styled.button`
+  font-size: 16px;
+  padding: 8px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  cursor: pointer;
+  display: inline-block;
+`;
+
+const CompletedButton = styled(Button)`
+  background-color: #22ee22;
+`;
+
+const RemoveButton = styled(Button)`
+  background-color: #ee2222;
+  margin-left: 8px;
+`;
+
 const TodoListItem = ({
   todo,
   onCompletedPressed,
   onRemovePressed,
-}: IProps) => (
-  <div className="todo-item-container">
-    <h3>{todo.text}</h3>
-    <div className="buttons-container">
-      {todo.isCompleted ? null : (
-        <button
-          className="completed-button"
-          onClick={() => onCompletedPressed(todo.id)}
+}: IProps) => {
+  const Container: any = todo.isCompleted
+    ? TodoItemContainer
+    : TodoItemContainerWithWarning;
+  return (
+    <Container createdAt={todo.createdAt}>
+      <h3>{todo.text}</h3>
+      <p>
+        Created at:&nbsp;
+        {new Date(todo.createdAt).toLocaleDateString()}
+      </p>
+      <ButtonsContainer>
+        {todo.isCompleted ? null : (
+          <CompletedButton
+            onClick={() => onCompletedPressed(todo.id)}
+            className="completed-button"
+          >
+            Mark As Completed
+          </CompletedButton>
+        )}
+        <RemoveButton
+          onClick={() => onRemovePressed(todo.id)}
+          className="remove-button"
         >
-          Mark As Completed
-        </button>
-      )}
-      <button
-        onClick={() => onRemovePressed(todo.id)}
-        className="remove-button"
-      >
-        Remove
-      </button>
-    </div>
-  </div>
-);
+          Remove
+        </RemoveButton>
+      </ButtonsContainer>
+    </Container>
+  );
+};
 
 export default TodoListItem;
